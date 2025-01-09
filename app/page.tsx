bcef78fe1d72f464e5c33ae9dfd58a89b0751450
NamanArora <namanarora.dev@gmail.com>
2025-01-09 20:18:10 +0530

    Add perf insights

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import TherapyLandingPage from './components/Hero';
 import Hero from './components/Hero';
 import SlotMachine from './components/Lottery';
 import OnboardingScreen from './components/OnboardingScreen';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
